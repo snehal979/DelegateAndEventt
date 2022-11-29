9fba53ad4421c05671b646faf035a0d51add94e2
Snehal979 <bansodsnehal535@gmail.com>
2022-11-29 20:04:44 +0530

    (Snehal)Add: multi cast delegate and array of delegate

--- DelegateAndEvent/DelegateAndEvent/Program.cs
+++ DelegateAndEvent/DelegateAndEvent/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace DelegateAndEvent
+﻿using System;
+
+namespace DelegateAndEvent
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("DelegateAndEvent");
-            Console.WriteLine("Enter a Number \n 1.single cast delegate");
+            Console.WriteLine("Enter a Number \n 1.single cast delegate\n 2.multi cast delegate \n 3.array delegate");
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
             {
@@ -15,6 +17,12 @@
                     Add a = new Add(creat.Add1);
                     a(100, 40);
                   
+                    break;
+                case 2:
+                    multiCastDelegate.ImplementationDelegate();
+                    break;
+                case 3:
+                    ArrayDelegate.ImplementationArrayDelegate();
                     break;
             }
         }
---
+++ DelegateAndEvent/DelegateAndEvent/multiCastDelegate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndEvent
+{
+    /// <summary>
+    /// MulticastDelegate
+    /// </summary>
+    public class multiCastDelegate
+    {
+        delegate void Object1(int x, int y);
+        public static void ImplementationDelegate()
+        {
+            Object1 obj = Calculation.Add1;
+            obj += Calculation.Sub1;
+            obj(75, 75);
+            obj(75, 75);
+
+
+        }
+    }
+    class Calculation
+    {
+
+        public static void Add1(int a, int b)
+        {
+            Console.WriteLine("sum is {0}", a + b);
+        }
+
+        public static void Sub1(int a, int b)
+        {
+            Console.WriteLine("sub is {0}", a - b);
+        }
+
+
+    }
+    /// <summary>
+    /// Array Delegate
+    /// </summary>
+    public class ArrayDelegate
+    {
+        delegate void Object1(int x, int y);
+        public static void ImplementationArrayDelegate()
+        {
+            // delegate instantation
+            Object1[] obj =
+            {
+                new Object1(Calculation.Add1),
+                new Object1(Calculation.Sub1)
+            };
+            for (int i = 0; i<obj.Length; i++)
+            {
+                obj[i](100, 60);
+                obj[i](34, 6);
+                obj[i](5, 4);
+                obj[i](7, 6);
+            }
+
+
+        }
+
+    }
+   
+    
+}
