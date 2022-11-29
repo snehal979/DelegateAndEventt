4ea2eaa9ac4bf09f33794febf0704f39bac38eb9
Snehal979 <bansodsnehal535@gmail.com>
2022-11-29 20:10:19 +0530

    (Snehal)Add: Event

--- DelegateAndEvent/DelegateAndEvent/Program.cs
+++ DelegateAndEvent/DelegateAndEvent/Program.cs
@@ -2,12 +2,14 @@
 
 namespace DelegateAndEvent
 {
+    public delegate void delegatevent();
     class Program
     {
+        public static event delegatevent add;
         public static void Main(string[] args)
         {
             Console.WriteLine("DelegateAndEvent");
-            Console.WriteLine("Enter a Number \n 1.single cast delegate\n 2.multi cast delegate \n 3.array delegate");
+            Console.WriteLine("Enter a Number \n 1.single cast delegate\n 2.multi cast delegate \n 3.array delegate \n 4.Event");
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
             {
@@ -24,6 +26,25 @@ namespace DelegateAndEvent
                 case 3:
                     ArrayDelegate.ImplementationArrayDelegate();
                     break;
+                case 4:
+                    add += new delegatevent(INDIA);
+                    add += new delegatevent(USA);
+                    add += new delegatevent(ENGLAND);
+                    add.Invoke();
+                    Event.Implementation();
+                    break;
+            }
+            static void INDIA()
+            {
+                Console.WriteLine("INDIA");
+            }
+            static void USA()
+            {
+                Console.WriteLine("USA");
+            }
+            static void ENGLAND()
+            {
+                Console.WriteLine("ENGLAND");
             }
         }
     }
